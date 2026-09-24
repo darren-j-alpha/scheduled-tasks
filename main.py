50f62afcb3fcf78f78ea19e915e52971403a121a
darren-j-alpha <jenkinson.darren@gmail.com>
2026-09-24 13:41:51 +0100

    main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import os
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
+TO_EMAIL = os.environ.get("TO_EMAIL")
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
@@ -28,11 +29,11 @@ if today_tuple in birthdays_dict:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
+            to_addrs=TO_EMAIL,
             msg=f"Subject:Happy Birthday!\n\n{contents}"
         )
